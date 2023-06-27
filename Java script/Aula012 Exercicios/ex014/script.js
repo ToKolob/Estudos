f8c713d45ed693b62f54ab581a3f2d49b0f4e599
ToKolob <111467715+ToKolob@users.noreply.github.com>
2023-06-27 15:45:52 -0300

    Adding 'que horas sao' exercise

--- Java script/Aula012 Exercicios/ex014/script.js
+++ Java script/Aula012 Exercicios/ex014/script.js
@@ -1,7 +1,10 @@
 
 function  carregar() {
     var mensagem = document.getElementById('mensagem')
-    var imagem = window.document
+    var img = window.document.getElementById('img')
+    var agora = new Date()
+    var horas = agora.getHours()
+    
 
 
 
@@ -9,5 +12,19 @@ function  carregar() {
 
 
 
-    mensagem.innerHTML(`São ${10} horas`)
+    mensagem.innerHTML = `São ${horas} horas`
+    if (horas < 18 && horas >= 12) {
+        mensagem.innerHTML = `Boa tarde, são ${horas} horas.`
+        img.src = 'tarde edit.png'
+        document.body.style.backgroundColor= 'orange'
+
+    }
+    else if (horas >= 6 && horas < 12 ) {
+        mensagem.innerHTML = `Bom dia. são ${horas} horas.`
+        document.body.style.backgroundColor = 'lightgreen'
+    }
+    else {
+        mensagem.innerHTML = `Boa noite, são ${horas} horas.`
+        document.body.style.backgroundColor = 'darkblue'
+    }
 }
