536c3e0a3719808d68e06fa9ce7221fc000a0640
ToKolob <111467715+ToKolob@users.noreply.github.com>
2023-08-01 10:12:53 -0300

    ok

--- Onebitcode/014 Visitando novas cidades/script.js
+++ Onebitcode/014 Visitando novas cidades/script.js
@@ -1,27 +1,31 @@
-let txnewCity = document.querySelector('input#newCity')
 const txres = document.querySelector('div#res')
 let spres = document.querySelector('span#res')
 const crlist = document.createElement('select')
 crlist.size = '10'
 crlist.style.width = '200px'
 
+const travelerName = prompt ('Nobre viajante, qual é seu nome?')
+
+let listOfCityies = 0
+
 function appendCity() {
-  if (txnewCity.value.length > 0) {
+
+  let knowNewCity = '1'
+
+  while (knowNewCity != '2') {
+    
+    let txnewCity = prompt ('Qual cidade você conhece?')
+
     let crnewCity = document.createElement('option')
-    crnewCity.textContent = txnewCity.value
+    crnewCity.textContent = txnewCity
     crlist.appendChild(crnewCity)
     txres.appendChild(crlist)
-    let listOfCityies = []
-    listOfCityies += txnewCity.value
+    listOfCityies ++
 
-    for (let city in listOfCityies){
-      spres.innerHTML += city
-    } 
+    spres.innerHTML = `O nobre viajante ${travelerName} conhece ${listOfCityies} cidades!`
 
-    
+    knowNewCity = prompt ('Você conheçe alguma nova cidade? (1)-Sim (2)-Não')
 
-  } else {
-    window.alert('Verifique os dados e tente novamente.')
+
   }
 }
-
